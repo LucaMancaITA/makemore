d9c2d87f62b5a9328d091c660efc0b10f070b552
LucaMancaITA <luca-manca@outlook.it>
2023-07-16 23:12:39 +0200

    first training loop

--- train.py
+++ train.py
@@ -1,35 +1,74 @@
 
+# Imports
 import json
-import random
-from utils import build_dataset
+import time
+import torch
+from torch.utils.data import DataLoader
+from utils import build_datasets
+from nets.bigram import Bigram
 
 
 # Hyperparameters
 with open("config/config.json", "r", encoding="utf-8") as config:
     config = json.load(config)
-block_size = config["block_size"]
+seed = config["seed"]
+vocab_size = config["vocab_size"]
+batch_size = config["batch_size"]
+max_epochs = config["max_epochs"]
+learning_rate = config["learning_rate"]
+weight_decay = config["weight_decay"]
 
-# Read all the words
-words = open("data/names.txt", "r").read().splitlines()
-print(f"Number of words: {len(words)}")
-
-# Build the vocabolary of characters and mapping to/from integers
-chars = sorted(list(set(''.join(words))))
-stoi = {s:i+1 for i,s in enumerate(chars)}
-stoi['.'] = 0
-itos = {i:s for s,i in stoi.items()}
-
-# Set the random seed
-random.seed(42)
-random.shuffle(words)
+# System inits
+torch.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
 
 # Generate train, validation and test set
-n1 = int(0.8*len(words))
-n2 = int(0.9*len(words))
-Xtr, Ytr = build_dataset(words[:n1], block_size, stoi)
-Xdev, Ydev = build_dataset(words[n1:n2], block_size, stoi)
-Xte, Yte = build_dataset(words[n2:], block_size, stoi)
-print("\nDataset info:")
-print(f"Training samples:     {Xtr.shape[0]}")
-print(f"Validation samples:   {Xdev.shape[0]}")
-print(f"Testing samples:      {Xte.shape[0]}")
+filename = "data/names.txt"
+train_dataset, val_dataset, test_dataset = build_datasets(filename)
+
+# Train dataloader
+train_dataloader = DataLoader(
+    train_dataset, batch_size=batch_size, shuffle=True)
+
+# Create the model
+model = Bigram(vocab_size=vocab_size)
+
+# Optimizer
+optimizer = torch.optim.AdamW(
+    model.parameters(),
+    lr=learning_rate,
+    weight_decay=weight_decay,
+    betas=(0.9, 0.99),
+    eps=1e-8
+)
+
+# Training loop
+print("\nTraining loop ...")
+for step in range(max_epochs):
+
+    t0 = time.time()
+
+    # Read next batch
+    batch = next(iter(train_dataloader))
+
+    x_batch, y_batch = batch
+
+    # Forward pass
+    logits, loss = model(x_batch, y_batch)
+
+    # Backward pass
+    model.zero_grad(set_to_none=True)  # as p.grad = None for p in parameters
+    loss.backward()
+    optimizer.step()
+
+    t1 = time.time()
+
+    # Logging
+    if step == 0 or step % 10 == 0:
+        print(f"Step {step} | loss {loss.item():.4f}" \
+              f"| step time {(t1-t0)*1000:.2f}ms")
+
+    # Model evaluation --> TODO
+
+    if step >= 500:
+      break
